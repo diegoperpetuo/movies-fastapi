155e3bdb584ef513f5c2dbbfd9eb545018c5983f
diegoperpetuo <diegoperpetuo2004@gmail.com>
2025-06-16 02:21:01 -0300

    Fix bugs

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Authorization"]
 )
 
 app.include_router(user_router, prefix="/api/auth", tags=["auth"])
